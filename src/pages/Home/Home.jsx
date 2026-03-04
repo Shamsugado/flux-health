958289f722361f98680a4040b558481251db6d09
Shamsu Gado <39752630+Shamsgado@users.noreply.github.com>
2026-03-04 07:28:11 -0500

    create top sellers section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Banner from './Banner'
+import TopSellers from './TopSellers'
+
 
 const home = () => {
   return (
     <div>
         <Banner/>
+        <TopSellers/>
         </div>
   )
 }
